4989db2b162fce0ae799be165914d20f58ad039b
Massa <nkl.massa@hotmail.it>
2016-03-06 13:56:06 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+while 1:
+  print("Select one of the following options:")
+  print("1: LogIn")
+  print("2: Add File")
+  print("3: Remove File")
+  print("4: Search File")
+  print("5: Download File")
+  print("6: LogOut")
+  option = input()
+  print("You've selected: " + option)
